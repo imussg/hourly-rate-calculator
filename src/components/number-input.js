360a0363505dd405d0046361b0a18e48f7db780b
Steven Imus <imussg@gmail.com>
2018-06-19 15:21:24 -0400

    finished 2nd one

--- src/components/number-input.js
+++ src/components/number-input.js
@@ -4,9 +4,7 @@ export default function NumberInput(props) {
     return (
         <div className="form-group">
             <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
+            <input type="number" id={props.id} min={props.min} max={props.max} value={props.value} onChange={e => props.onChange(e)}/>
         </div>
     );
 }
-
-
--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,16 +6,37 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            hours:8,
+            dayRate: 300
+        };
+    }
+
+    setHours(hours) {
+        console.log(hours);
+        this.setState({ hours });
+        this.render();
+    }
+
+    setDayRate(dayRate) {
+        console.log(dayRate);
+        this.setState({ dayRate });
     }
 
     render() {
-        const rate = 10;
+
+        const rate = Number(this.state.dayRate) / Number(this.state.hours);
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                <NumberInput id="day-rate" label="Day rate" min={0} max={5000}
+                    onChange={ e=>this.setDayRate(e.target.value) }
+                    value = {this.state.dayRate}
+                />
+                <NumberInput id="hours" label="Hours" min={1} max={12}
+                    onChange = { e => this.setHours(e.target.value) }
+                    value = {this.state.hours}
+                />
+                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}/>
             </form>
         );
     }
